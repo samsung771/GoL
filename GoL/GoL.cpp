c232884d8e662b522eb690645901f32c6533e829
Sam Leech <sam.d.leech@gmail.com>
2021-06-18 14:06:58 +0100

    commit

--- GoL/GoL.cpp
+++ GoL/GoL.cpp
@@ -2,16 +2,22 @@
 #include <time.h>
 #include <vector>
 #include <string>
+#include <Windows.h>
 #include <stdlib.h>
+#include <chrono>
+#include <thread>
 
 #define DIMX 60
-#define DIMY 30
+#define DIMY 29
+#define frameTime 10
 
 void printBoard(bool (&board)[DIMY][DIMX]) {
 	std::string print;
 
+	//iterates through each square in the grid
 	for (int y = 0; y < DIMY; y++) {
 		for (int x = 0; x < DIMX; x++) {
+			//makes it a pixel either 2 spaces or 2 squares if it is alive or dead
 			char square = 219;
 			std::string letter;
 			if (board[y][x] == 0) letter = "  ";
@@ -25,16 +31,17 @@ void printBoard(bool (&board)[DIMY][DIMX]) {
 }
 
 void popBoard(bool (&board)[DIMY][DIMX]) {
+	//fills the board with a 50% chance of each one being alive
 	srand((unsigned)time(NULL));
 	for (int y = 0; y < DIMY; y++) {
 		for (int x = 0; x < DIMX; x++) {
 			board[y][x] = rand() % 2;
 		}
-	} 
+	}
 }
 
 void updateBoard(bool(&board)[DIMY][DIMX]) {
-	bool newBoard[DIMX][DIMY] = {0};
+	bool newBoard[DIMY][DIMX] = {0};
 	for (int y = 0; y < DIMY; y++) {
 		for (int x = 0; x < DIMX; x++) {
 			std::vector<bool*> neighbors;
@@ -70,14 +77,17 @@ void updateBoard(bool(&board)[DIMY][DIMX]) {
 }
 
 int main() {
+	static HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
 	bool board[DIMY][DIMX] = {0};
 
 	popBoard(board);
-
 	while (true) {
 		printBoard(board);
 		updateBoard(board);
-		//system("CLS");
+
+		//sleep till next frame
+		std::this_thread::sleep_for(std::chrono::milliseconds(frameTime));		
+		SetConsoleCursorPosition(hConsole, COORD{0,0});
 	}
 }
-
